72941b8129bacc003f9cd8dd4d0550b2b0eafb2c
Samuel Martinez <semartinez147@gmail.com>
2020-07-22 13:38:45 -0600

    Formatted comments

--- src/com/semartinez/Arrays.java
+++ src/com/semartinez/Arrays.java
@@ -3,10 +3,10 @@ package com.semartinez;
 public class Arrays {
 
   /**
-   *https://codingbat.com/prob/p189576
+   * https://codingbat.com/prob/p189576
    * Consider the leftmost and righmost appearances of some value in an array. We'll say that the
-   "span" is the number of elements between the two inclusive. A single value has a span of 1.
-   Returns the largest span found in the given array. (Efficiency is not a priority.)
+   * "span" is the number of elements between the two inclusive. A single value has a span of 1.
+   * Returns the largest span found in the given array. (Efficiency is not a priority.)
    */
   public int maxSpan(int[] nums) {
     if (nums.length == 0) {
--- src/com/semartinez/Strings.java
+++ src/com/semartinez/Strings.java
@@ -3,10 +3,10 @@ package com.semartinez;
 public class Strings {
 
   /**
-   * https://codingbat.com/prob/p199171 Given a string, count the number of words ending in 'y' or
-   * 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count, but not the 'y' in "yellow" (not case
-   * sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
-   * letter immediately following it.
+   * https://codingbat.com/prob/p199171
+   * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the
+   * 'z' in "fez" count, but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z
+   * is at the end of a word if there is not an alphabetic letter immediately following it.
    */
   public int countYZ(String str) {
     int count = 0;
@@ -40,10 +40,8 @@ public class Strings {
   }
 
   /**
-   * https://codingbat.com/prob/p179479
-   *
-   Given a string, return the length of the largest "block" in the string. A block is a run of
-   adjacent chars that are the same.
+   * https://codingbat.com/prob/p179479 Given a string, return the length of the largest "block" in
+   * the string. A block is a run of adjacent chars that are the same.
    */
   public int maxBlock(String str) {
     if (str.length() == 0) {
